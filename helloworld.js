bd040dde1cb5ef7809e1b7efa1e7b2b7f0d772df
Eli Livshitz <eli.livshitz@gmail.com>
2018-04-09 02:42:02 +0300

    Update helloworld.js

--- helloworld.js
+++ helloworld.js
@@ -1,4 +1,4 @@
 var x = 'your config';
-var version = 12;
+var version = 13;
 var newConfig = 'yes!';
 console.log('Hello world! Testing SuperWebApp shared infrastracture - ' + version + ' - ' + x);
